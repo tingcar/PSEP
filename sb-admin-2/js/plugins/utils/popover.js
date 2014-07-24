8c8c8ad66f1c93b0c95ea0b43fbed0794ed1387b
Jiayu Chen <tingcar@gmail.com>
2014-07-24 17:59:53 +0800

    template version 0.01

--- sb-admin-2/js/plugins/utils/popover.js
+++
@@ -1,2 +0,0 @@
-$(function(){
-	$(".h5a-sidebar").affix({offset:{top:-10,bottom:function(){return this.bottom=$(".bs-footer").outerHeight(!0)}}}),$(".popover-test").popover({trigger:"hover",html:!0}),$(".tooltip-test").tooltip({html:!0})});
